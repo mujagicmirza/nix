529bb8b6bd7f7173fb5e3e237b2cd1c61a6ffc7b
Mirza Mujagić <m.mujagic@jobstep.com>
2023-06-25 07:17:21 +0200

    Add system-wide configs

--- xserver.nix
+++ xserver.nix
@@ -23,4 +23,31 @@ in
       enable = true;
     };
   };
+
+  virtualisation.docker.enable = true;
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+      };
+    };
+  };
 }
